0db8f15690fff749236851c7f521e22c864b6737
Daniel Arribas <93707372+dani78w@users.noreply.github.com>
2024-05-20 21:51:09 +0200

    add-feature(bottom bar navigation)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './screens/FeedScreen'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import AddCircleIcon from '@mui/icons-material/AddCircle';import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import HomeIcon from '@mui/icons-material/Home';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import NotaFormulario from './components/NotaFormulario';
 import ListaEncuestas from './components/ListaEncuestas'; 
@@ -15,30 +15,40 @@ import EncuestaScreen from './screens/EncuestaScreen';
 function App() {
   return (
     <Router>
-      <Routes>
-              
-        <Route path="/" element={ <FeedScreen/> } />
-        <Route path="/new" element={ <CreadorEncuesta/> } />
-        <Route path="/encuesta/:id" element={<EncuestaScreen />} />
-      </Routes>
-      <div className='fixed bottom-0 w-full shadow-lg z-50'>
-        <BottomNavigation
-         sx={{
-          backgroundColor: 'rgba(255, 255, 255, 0.5)',
-          backdropFilter: 'blur(10px)'// Añade un borde difuso
-        }}
-            showLabels
-            onChange={(event, newValue) => {
-            
-            }}
-          >
-            <BottomNavigationAction label="Recientes" icon={<FavoriteIcon />} />
-            <BottomNavigationAction  label="Nueva" icon={<AddCircleIcon />} />
-            <BottomNavigationAction label="Cercanas" icon={<LocationOnIcon />} />
-          </BottomNavigation>
+      <div>
+        <Routes>
+          <Route path="/" element={<FeedScreen />} />
+          <Route path="/new" element={<CreadorEncuesta />} />
+          <Route path="/encuesta/:id" element={<EncuestaScreen />} />
+        </Routes>
+        <BottomNav />
       </div>
-      
     </Router>
+  );
+}
+
+function BottomNav() {
+  const navigate = useNavigate();
+  
+  return (
+    <div className='fixed bottom-0 w-full shadow-lg z-50'>
+      <BottomNavigation
+        sx={{
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+          backdropFilter: 'blur(10px)' // Añade un borde difuso
+        }}
+        showLabels
+        onChange={(event, newValue) => {
+          if (newValue === 0) navigate('/');
+          if (newValue === 1) navigate('/new');
+          if (newValue === 2) navigate('/nearby');
+        }}
+      >
+        <BottomNavigationAction label="Inicio" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Nueva" icon={<AddCircleIcon />} />
+        <BottomNavigationAction label="Cercanas" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </div>
   );
 }
 
--- src/screens/CreadorEncuesta.jsx
+++ src/screens/CreadorEncuesta.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import NotaFormulario from '../components/NotaFormulario';
+import { Button } from '@nextui-org/react';
 
 
 function CreadorEncuesta(){
+    const variants = ["flat", "bordered", "underlined", "faded"];
+
     return(
-        <NotaFormulario/>
+        <div>
+            <Button>Click me</Button>
+
+            <NotaFormulario/>
+        </div>
+        
     );
 }
 
